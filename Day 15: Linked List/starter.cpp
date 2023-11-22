927c3df90585c43b0091d2954bb63fdf0118ecbd
Gabi Anderson <54378358+GabiAnderson@users.noreply.github.com>
2023-11-22 10:38:36 -0700

    added starter code day 15

---
+++ Day 15: Linked List/starter.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstddef>
+using namespace std;	
+class Node
+{
+    public:
+        int data;
+        Node *next;
+        Node(int d){
+            data=d;
+            next=NULL;
+        }
+};
+class Solution{
+    public:
+// DO NOT CHANGE CODE ABOVE
+      Node* insert(Node *head,int data)
+      {
+          //Complete this method
+      }
+// DO NOT CHANGE CODE BELOW
+      void display(Node *head)
+      {
+          Node *start=head;
+          while(start)
+          {
+              cout<<start->data<<" ";
+              start=start->next;
+          }
+      }
+};
+int main()
+{
+	Node* head=NULL;
+  	Solution mylist;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        head=mylist.insert(head,data);
+    }	
+	mylist.display(head);
+		
+}
